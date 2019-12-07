38f175a6d2db36b02c16c1a52edfebc83b1a4ebc
Dagmara <dagmarakitkowska@gmail.com>
2019-12-07 14:18:08 +0100

    Another commit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.util.Scanner;
 
 /**
  * ...comment class...
@@ -7,24 +6,4 @@ import java.util.Scanner;
  * @since 07.12.2019
  */
 final class Main {
-
-    public static void main(String[] args) {
-        System.out.println("WITAJCIE");
-           while (true) {
-            System.out.println("Gdzie idziemy jesć");
-            Scanner pytanie = new Scanner(System.in);
-            System.out.println("Podaj nazwę miejsca");
-            String pytanieOdp = pytanie.nextLine();
-            if(!pytanieOdp.equals("Gdziekolwiek")){
-                break;
-//            }
-//            System.out.println("To zbierajmy tylek? TAK/NIE");
-//            Scanner pytanie2 = new Scanner(System.in);
-//            String pytanieOdp2 = pytanie2.nextLine();
-//            if(pytanieOdp2.equals("Tak")) {
-//                break;
-            }
-        }
-        System.out.println("Powodzenia!");
-    }
 }
