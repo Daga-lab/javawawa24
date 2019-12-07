7d9e76dc7354aed519af48ce32d7ef8fd1d5b9d8
PiotrCzechowski <czechowp@gmail.com>
2019-12-07 15:25:11 +0100

    zmiana piotrka

--- src/Main.java
+++ src/Main.java
@@ -10,9 +10,17 @@
 public final class Main {
 
     public static void main(String[] args) {
+
+        Prostokat prostokat = new Prostokat(10,5);
+        System.out.println(prostokat.Obwod());
+        System.out.println(prostokat.toString());
+
+        wyswietlImie();
+
+        pierwszeLiteryAlfabetu();
     }
 
-    public static void pierwszeLiteryAlfabetu (){
+    public static void  pierwszeLiteryAlfabetu (){
         String [] alphabet = new String [10];
         alphabet[0] = "A";
         alphabet[1] = "B";
@@ -20,11 +28,17 @@ public final class Main {
         alphabet[3] = "D";
         alphabet[4] = "E";
 
+        for (int i = 0; i<5; i++ ) {
+            System.out.println(alphabet[i]);
+        };
     }
 
     public static void wyswietlImie () {
         System.out.println("Piotr");
+
+
     }
+
 
 
 }
