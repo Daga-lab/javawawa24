e123bcb9e136f38e6fa345657e4b4fcba539bed0
Maciek <maciej.jasinski2@gmail.com>
2019-12-07 15:36:15 +0100

    poprawione metody

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import java.sql.SQLOutput;
-
 /**
  * ...comment class...
  *
@@ -19,21 +17,29 @@ public final class Main {
         prostokat.DlugoscPrzekatnej();
         System.out.println("Dlugosc przekatnej prostokata " + prostokat.DlugoscPrzekatnej());
 
+        wyswietlImie();
+
+        pierwszeLiteryAlfabetu();
     }
 
-
-    public static void pierwszeLiteryAlfabetu (){
-        String [] alphabet = new String [10];
+    public static void pierwszeLiteryAlfabetu() {
+        String[] alphabet = new String[10];
         alphabet[0] = "A";
         alphabet[1] = "B";
         alphabet[2] = "C";
         alphabet[3] = "D";
         alphabet[4] = "E";
 
+        for (int i = 0; i < 5; i++) {
+            System.out.println(alphabet[i]);
+        }
+        ;
     }
 
-    public static void wyswietlImie () {
+    public static void wyswietlImie() {
         System.out.println("Piotr");
+
+
     }
 
 
